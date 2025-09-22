0e88b3f9037be1ae00a907c77aa6046ddabf1704
sunny-io <83395990+sunny-io@users.noreply.github.com>
2025-09-22 14:01:32 +0200

    Login-Test
    still unfinished. assertions for failed cases and testcases for fail still missing

--- Classes/Tests/login_test.js
+++ Classes/Tests/login_test.js
@@ -1,6 +1,6 @@
 //requirements node
 const assert = require("node:assert");
-const { WebDriver, until, Builder, Key } = require("selenium-webdriver");
+const { WebDriver, until, Builder, Key, By } = require("selenium-webdriver");
 
 require("chromedriver");
 
@@ -8,11 +8,13 @@ require("chromedriver");
 const pageObject = {
   baseUrl: "https://practicetestautomation.com/practice-test-login/",
   main: 'section[id="login"]',
-  inputFields: [
-    { username: { locatortype: "id", locator: "username" } },
-    { password: { locatortype: "id", locator: "password" } },
-  ],
-  buttons: [{ submit: { locatortype: "id", locator: "submit" } }],
+  inputFields: {
+    username: { locatortype: "id", locator: "username" },
+    password: { locatortype: "id", locator: "password" },
+  },
+
+  buttons: { submit: { locatortype: "id", locator: "submit" }, logout: {} },
+  errorMsgs: { locatortype: "id", locator: "error"},
 };
 
 //testcases
@@ -21,32 +23,57 @@ testcases = [
     testid: "loginSuccess",
     baseUrlTitle: "Test Login | Practice Test Automation",
     action: "fillAndSubmitForm",
-    data: [
-      { locator: pageObject.inputFields.username, content: "student" },
-      { locator: pageObject.inputFields.password, content: "Password123" },
-    ],
+    data: {
+      username: {
+        locator: pageObject.inputFields.username,
+        content: "student",
+      },
+      password: {
+        locator: pageObject.inputFields.password,
+        content: "Password123",
+      },
+    },
     submitBtn: pageObject.buttons[0],
+    resultPageObject: {
+      resultUrl: "practicetestautomation.com/logged-in-successfully/",
+      resultUrlTitle: "Logged In Successfully | Practice Test Automation",
+      resultItems: {
+        firstTitle: { locatortype: "xpath", locator: "//h1.post-title" },
+        resultButton: {
+          locatortype: "xpath",
+          locator: "//a.wp-block-button__link[text()='Log out']",
+        },
+      },
+    },
   },
 ];
 
 // reusable function to fill a field. Currently only locatortype id supported
-async function fillField(locator, content) {
+async function fillField(dataset, driver) {
   console.log("started fillField");
-  if (locator.locatortype == "id") {
+  //console.log(JSON.stringify(dataset));
+  if (dataset.locator.locatortype == "id") {
     try {
+      console.log("started try fillField");
       await driver
-        .findElement({ id: locator.locator })
-        .sendKeys(content, Key.ENTER);
+        .findElement({ id: dataset.locator.locator })
+        .sendKeys(dataset.content, Key.ENTER);
       await driver.wait(
-        until.findElement({ id: locator.locator }).value == content,
+        until.text_to_be_present_in_element_value(
+          { id: dataset.locator.locator } == dataset.content
+        ),
         1000
       );
-      console.log(await driver.findElement({ id: locator.locator }).value);
+      console.log(
+        await driver.findElement({ id: dataset.locator.locator }).value
+      );
       assert.equal(
-        await driver.findElement({ id: locator.locator }).value,
+        await driver.findElement({ id: dataset.locator.locator }).value,
         content
       );
-      console.log(`field ${locator.locator} filled with ${content}`);
+      console.log(
+        `field ${dataset.locator.locator} filled with ${dataset.content}`
+      );
     } catch (e) {
       console.log(`error in fillField ${e}`);
     }
@@ -54,28 +81,72 @@ async function fillField(locator, content) {
 }
 
 // function to loop through all input fields in the test case
-async function fillFields(data) {
+async function fillFields(data, driver) {
   try {
-    for (i = 0; i < data.length; i++) {
-      await fillField(data.locator, data.content);
+    for (const key of Object.keys(data)) {
+      //console.log(JSON.stringify(data[key]));
+      await fillField(data[key], driver);
     }
   } catch (e) {
     console.log(`error in fillFields ${e}`);
   }
 }
 
-async function executeTestLogic(testcase) {
-  if (testcase.action == "fillAndSubmitForm") {
+async function assertResult(testcase, driver) {
+  if (testcase.resultPageObject) {
     try {
-      await fillFields(testcase.data);
+      //assert new page result
+      await driver.wait(
+        until.urlContains(testcase.resultPageObject.resultUrl),
+        1000
+      );
+      assert.ok(
+        (await driver.getCurrentUrl()).includes(
+          testcase.resultPageObject.resultUrl
+        )
+      );
+      assert.ok(
+        (await driver.getTitle()).includes(
+          testcase.resultPageObject.resultUrlTitle
+        )
+      );
+
+      assert.ok(
+        await driver.findElement(
+          By.xpath(testcase.resultPageObject.resultButton.locator)
+        )
+      );
+      assert.ok(
+        await driver.findElement(
+          By.xpath(testcase.resultPageObject.firstTitle.locator)
+        )
+      );
+    } catch (e) {
+      console.log(`error while asserting resultsPage ${e}`);
+    }
+    } else {
+        assert.ok (
+            (await driver.findElement( {xpath: pageObject.errorMsgs })
+            );
+    }
+}
+
+async function executeTestLogic(testcase, driver) {
+  console.log("executeTestLogic started");
+  if (testcase.action == "fillAndSubmitForm") {
+    console.log(`testcase action: ${testcase.action}`);
+    try {
+      console.log(`testcase.data: ${JSON.stringify(testcase.data)}`);
+      await fillFields(testcase.data, driver);
     } catch (e) {
       console.log(`error in executeTestLogic ${e}`);
     }
     try {
-      driver.findElement({ id: "submit" }).click(); //fixed id for now
+      await driver.findElement({ id: "submit" }).click();
     } catch (e) {
       console.log(`error in clicking Submit Button ${e}`);
     }
+    assertResult(testcase, driver);
   }
 }
 
@@ -87,12 +158,13 @@ async function runTest(testcase) {
     driver = await new Builder().forBrowser("chrome").build();
     await driver.get(pageObject.baseUrl);
     assert.equal(await driver.getTitle(), testcase.baseUrlTitle);
+    console.log(await driver.getTitle());
 
-    await executeTestLogic(testcase);
+    await executeTestLogic(testcase, driver);
   } catch (e) {
     console.log(e);
   } finally {
-    await driver.quit();
+    //await driver.quit();
   }
 }
 
